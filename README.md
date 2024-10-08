be1e3d599ae4f2ba2d7293a1611515bdbcf8f400
kmTyagi198 <k.m.tyagi198@gmail.com>
2024-10-09 03:01:40 +0530

    added some animations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Developed basic todo app follwing redux toolkit tutorial
 - This repo stores the basic project i developed when learning react redux toolkit
+- uses tailwindCSS, framer motion, react icons
+- tasks persist on reload
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import AddTodo from './components/addTodo/AddTodo'
 import TodoList from './components/todoList/TodoList'
+import { motion } from 'framer-motion';
 import './index.css'
 
 function App() {
 
-
   return (
-    <>
-    <div className=' m-4 md:m-10 bg-[#27005D]   text-white rounded-[28px] font-serif'>
+    <motion.div
+    initial={{ y: 50, opacity: 0 }} // Start 50px below the original position and invisible
+    animate={{ y: 0, opacity: 1 }}  // Animate to the default position and full opacity
+    transition={{ duration: 0.8, ease: 'easeOut' }} // Control the animation speed and easing
+  >
+    <div className=' mt-10 m-4 md:m-10 bg-[#27005D]   text-white rounded-[28px] font-serif'>
         <div className=' mb-6'>
             <h1 className='text-center text-4xl'>Basic Todo</h1>
         </div>
@@ -19,7 +23,7 @@ function App() {
             <TodoList/>
         </div>
     </div>
-    </>
+    </motion.div>
   )
 }
 
